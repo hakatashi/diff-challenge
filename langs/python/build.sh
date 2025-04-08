3f70f617903e47679bad7b9eaa5b9ba9285a972b
Koki Takahashi <hakatasiloving@gmail.com>
2025-04-08 11:53:21 +0000

    Fix build script by adding missing cleanup command for compiled Python bytecode

--- langs/python/build.sh
+++ langs/python/build.sh
@@ -3,3 +3,4 @@
 cp -f "$1" /tmp/code.py
 ~/.pyenv/versions/3.12.2/bin/python -m py_compile /tmp/code.py
 cp -f /tmp/__pycache__/code.cpython-312.pyc "$2"
+rm -f /tmp/__pycache__/code.cpython-312.pyc
